8969e8f1b402d5a68e567b125fe09a3b81f38308
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-16 14:57:54 -0400

    Daylight Savings Time and Leap Year logic work

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const chalk = require('chalk');
 let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 let dayOfYear = moment().format('DDDD');
 let secondsOfTheDay = moment().format('ss');
-let daylightSavingsTime = moment().isDST();
-let leapYear = if(moment().isLeapYear(){
-  "is"
+let daylightSavingsTime = '';
+let leapYear = '';
+if(moment().isDST()){
+  daylightSavingsTime = "is";} else {daylightSavingsTime = "is not";
+};
+if(moment().isLeapYear()){
+  leapYear = "is";} else {leapYear = "is not";
 };
 
 
 console.log("It is " + currentTime + ".");
 console.log("It is the " + dayOfYear + " day of the year.");
 console.log("It is currently " + secondsOfTheDay + " seconds into the day.");
-console.log("It is currently " + daylightSavingsTime + " daylight savings time.");
+console.log("It " + daylightSavingsTime + " currently daylight savings time.");
 console.log("It " + leapYear + " a leap year.");
